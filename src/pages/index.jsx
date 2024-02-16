f4f07ca3384bf4885313cf89966de42d8105fc27
HazelDaniel <hazeldaniel856@gmail.com>
2024-02-16 16:51:12 +0100

    removed the oauth logic handling from the index page. made a configuration file that informs vercel on how to handle the path of my SPA

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,21 +1,9 @@
-import { useEffect } from "react";
 import { About } from "../components/about";
 import { Explore } from "../components/explore";
 import { Hero } from "../components/hero";
 import { Motto } from "../components/motto";
-import { useNavigate } from "react-router-dom";
 
 export const Index = () => {
-  const queryString = window.location.search;
-  const urlParam = new URLSearchParams(queryString);
-  let codeParam = urlParam.get("code");
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (codeParam) {
-      navigate(`/app?code=${codeParam}`, { replace: true });
-    }
-  }, []);
 
   return (
     <>
