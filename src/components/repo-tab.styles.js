fc11ccfbed14928774c5ba0770347c8750681d3b
HazelDaniel <hazeldaniel856@gmail.com>
2024-02-18 16:45:07 +0100

    improved the styling of the repo list

--- src/components/repo-tab.styles.js
+++ src/components/repo-tab.styles.js
@@ -1,4 +1,5 @@
 import {
+    debug,
   makeAbsoluteBottomDiv,
   makeAbsoluteBottomRightDiv,
   makeAbsoluteTopDiv,
@@ -11,6 +12,7 @@ import {
   makeFullWidthBlock,
   makeRowFlexCenter,
   makeRowFlexStart,
+  removeTapHighlight,
 } from "../styles/functions";
 import styled from "styled-components";
 
@@ -30,6 +32,7 @@ export const RepoTabStyled = styled.aside`
   width: 33rem;
   @media screen and (max-width: 800px) {
     position: fixed;
+		width: 80vw;
 
     // ====== a hiding class
     &.closed {
@@ -132,15 +135,18 @@ export const RepoTabStyled = styled.aside`
     margin-bottom: 20%;
     ${makeRowFlexStart};
     gap: 30%;
+		${removeTapHighlight};
     & > div {
-      width: 10rem;
+			width: 12rem;
       button {
         ${makeFullWidthBlock};
-        font-size: 1.2rem;
         ${makeRowFlexCenter};
         align-items: center;
         gap: 1rem;
-        height: 2rem;
+        height: 3rem;
+				/* font-size: 2rem; */
+					/*leave me  here*/
+				font-size: 20px;
         position: relative;
         cursor: pointer;
         font-family: manjari_regular;
@@ -148,8 +154,14 @@ export const RepoTabStyled = styled.aside`
         background-color: unset;
         outline: unset;
         border: unset;
+				transition: all .2s linear;
+				transition-property: border;
+				&:active {
+					border: .1rem solid var(--bright-color);
+				}
         &.blurred {
           filter: blur(2rem);
+					opacity: .8;
           background-color: var(--accent-color_trans);
         }
         &::before {
@@ -165,11 +177,11 @@ export const RepoTabStyled = styled.aside`
           }
         }
         span {
-          font-size: 2rem;
+          font-size: 2.5rem;
           line-height: 0;
           ${makeFullHeightBlock};
           ${makeAbsoluteTopRightDiv};
-          top: 1.5rem;
+          top: 2.45rem;
           align-items: center;
           transform: scale(1.8);
           right: -20%;
@@ -177,6 +189,19 @@ export const RepoTabStyled = styled.aside`
         }
       }
     }
+		.left {
+			&> button {
+				&>span {
+          top: 2.3rem;
+					right: -40%;
+				}
+			}
+			&:hover {
+				&>button > span {
+					right: -30%;
+				}
+			}
+		}
     .right {
       & > button {
         & > span {
@@ -195,5 +220,6 @@ export const RepoTabStyled = styled.aside`
         }
       }
     }
+		
   }
 `;
